1ab821c2a2243f1b02dcd17f493a846bd51413cb
yaroslav <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-08-25 15:24:25 +0300

    Created UsersService, added result pattern implementation

---
+++ Api/TodoApi/src/ToDo.API/Features/Users/Services/UserService/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using ToDo.API.Infrastructure.Entities;
+using ToDo.API.Wrappers.Result;
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace ToDo.API.Features.Users.Services.UserService;
+
+public interface IUserService
+{
+    public Task<Result<string>> Login(string email, string password);
+    
+    public Task<Result<User>> Register(User user);
+    
+    public Task<Result<User>> Update(User user);
+    
+    public Task<IResult> Delete();
+}
---
+++ Api/TodoApi/src/ToDo.API/Features/Users/Services/UserService/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.API.Infrastructure;
+using ToDo.API.Infrastructure.Entities;
+using ToDo.API.Wrappers.Result;
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace ToDo.API.Features.Users.Services.UserService;
+
+public class UserService : IUserService
+{
+    private readonly DbSet<User> _users;
+    private readonly AppDbContext _context;
+
+    public UserService(AppDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _users = context.Users;
+    }
+    
+    
+    public async Task<Result<string>> Login(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result<User>> Register(User user)
+    {
+        if(user == null)
+            return Result<User>.Fail("User is null");
+        
+        var registeredUser = await _users.AddAsync(user);
+        
+        return await Result<User>.SuccessAsync(user);
+    }
+
+    public async Task<Result<User>> Update(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IResult> Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
