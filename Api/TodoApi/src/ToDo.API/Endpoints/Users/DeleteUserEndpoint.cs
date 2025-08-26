c07e32693323a9145a61cd4ba8e1351fed6f8a9f
yaroslav <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-08-26 15:24:41 +0300

    fixed url of delete user endpoint

--- Api/TodoApi/src/ToDo.API/Endpoints/Users/DeleteUserEndpoint.cs
+++ Api/TodoApi/src/ToDo.API/Endpoints/Users/DeleteUserEndpoint.cs
@@ -1,10 +1,19 @@
-﻿using ToDo.API.Endpoints.Base;
+﻿using MediatR;
+using ToDo.API.Endpoints.Base;
 using ToDo.API.Features.Users.Commands.DeleteUserCommand;
 
 namespace ToDo.API.Endpoints.Users;
 
 public class DeleteUserEndpoint : BaseEndpoint<DeleteUserCommand>
 {
-    public override string EndpointUrl { get; } = "/users";
+    public override string EndpointUrl { get; } = "/users/{id:int}";
     public override EndpointHttpMethod EndpointHttpMethod { get; } =  EndpointHttpMethod.Delete;
+
+    protected override Delegate ConfigureHandler()
+    {
+        return async (int id,  IMediator mediator, CancellationToken cancellationToken) =>
+        {
+            return await base.HandleAsync(new (){ UserId = id}, mediator, cancellationToken);
+        };
+    }
 }
