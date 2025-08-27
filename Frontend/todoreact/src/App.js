61a56946a40fc6b7d7b88cbdb50932bb9d80210c
yaroslav <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-08-27 15:11:20 +0300

    Started User profile page

--- Frontend/todoreact/src/App.js
+++ Frontend/todoreact/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {Login} from "./components/login/login";
 import {Register} from "./components/register/register";
+import {Profile} from "./components/profile/profile";
 
 function App() {
   return (
     <div className="App">
-        <Register/>
+        <Profile/>
     </div>
   );
 }
---
+++ Frontend/todoreact/src/components/profile/profile.jsx
@@ -0,0 +1,34 @@
+import styles from "./profile.module.css";
+import {Fragment, useState} from "react";
+
+export const Profile = () => {
+    const [userInfo, setUserInfo] = useState({
+        id: 2,
+        name: "name",
+        surname: "surname",
+        email: "email",
+        tasks: []
+    });
+
+    return (<form>
+        <title>User profile</title>
+        <section className={styles.UserInfoCard}>
+            <h1> User info </h1>
+            <p>User: {userInfo.name} {userInfo.surname}</p>
+            <p>Email: {userInfo.email}</p>
+        </section>
+
+        <section className={styles.TasksCard}>
+            <h1> Tasks </h1>
+
+            <ul>
+                {userInfo.tasks.map((task) => (
+                    <li key={task.id}>
+                        {task}
+                    </li>
+                ))}
+            </ul>
+        </section>
+    </form>);
+
+}
